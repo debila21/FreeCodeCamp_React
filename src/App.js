6695bab0aad762be0cb26e736f46658c1974d5ba
Debila Thankaraj <debila21@gmail.com>
2023-03-24 14:09:43 -0400

    Random Quote Generator

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import { useEffect,useState } from "react";
+import './App.css';
+
+
+function App() {
+  const [quoteInfo,setQuoteInfo]=useState({});
+
+useEffect (()=>{
+getQuote();
+},[]);
+
+const getQuote =()=>{
+  fetch('https://api.quotable.io/random')
+  .then((response)=>{return response.json();
+  })
+  .then((data)=>{
+    setQuoteInfo({
+      text:data.content,
+      author:data.author,
+    });
+   
+  });
+};
+
+
+  return (
+    <div className="App">     
+     <div id="quote-box">
+      <p id="text">{quoteInfo.text}</p>
+      <p id="author">{quoteInfo.author}</p>
+      <button id="new-quote" onClick={getQuote}>New Quote
+      </button>
+      <a href={'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + quoteInfo} id="tweet-quote">Post to twitter</a>
+
+     </div>
+    </div>
+  );
+}
+
+export default App;
